defb53144765796dd8927eb2a4d5b8458fa8b064
No-Hyunji <kiekuy47@gmail.com>
2020-09-21 12:27:31 +0900

    ReadBook

--- SpMVC_ReadBook/src/main/java/com/biz/book/config/NaverSecretSample.java
+++ SpMVC_ReadBook/src/main/java/com/biz/book/config/NaverSecretSample.java
@@ -2,6 +2,14 @@ package com.biz.book.config;
 
 public class NaverSecretSample {
 	
-	public final static String NAVER_CLIENT_ID = "Nml7mm6ysFaXsVMPHiea";
-	public final static String NAVER_CLIENT_SECRET = "4_1XoqtDnm";
+	/*
+	 * 네이버 OpenAPI 등록을 수행 한 후 
+	 * Client ID와 Client Secret를 복사하여 등록 한 후 
+	 * 이 클래스를 NaverSecret로 변경하여 프로젝트를 실행하시오
+	 */
+	public final static String NAVER_CLIENT_ID = "YOUR NAVER CLIENT ID";
+	public final static String NAVER_CLIENT_SECRET = "YOUR NAVER SECRET";
+	
+	public final static String NAVER_BOOK_XML = "https://openapi.naver.com/v1/search/book.xml";
+	public final static String NAVER_BOOK_JSON = "https://openapi.naver.com/v1/search/book.json";
 }
