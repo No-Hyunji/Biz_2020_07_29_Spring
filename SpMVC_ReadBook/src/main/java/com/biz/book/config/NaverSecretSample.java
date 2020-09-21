716cea428c32b2d96cab1fac7212ebad375943c1
No-Hyunji <kiekuy47@gmail.com>
2020-09-21 10:18:46 +0900

    ReadBook

---
+++ SpMVC_ReadBook/src/main/java/com/biz/book/config/NaverSecretSample.java
@@ -0,0 +1,7 @@
+package com.biz.book.config;
+
+public class NaverSecretSample {
+	
+	public final static String NAVER_CLIENT_ID = "Nml7mm6ysFaXsVMPHiea";
+	public final static String NAVER_CLIENT_SECRET = "4_1XoqtDnm";
+}
