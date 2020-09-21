bdf5746f556cbe7a924264b0d4ea293fc2eb23d3
No-Hyunji <kiekuy47@gmail.com>
2020-09-21 10:12:32 +0900

    ReadBook

---
+++ SpMVC_ShopV4/src/main/java/com/biz/shop/exec/EncryptExec.java
@@ -0,0 +1,127 @@
+package com.biz.shop.exec;
+
+public class EncryptExec {
+	public static void main(String[] args) {
+		
+		// 평문을 일방향 암호화를 하는 클래스 도구
+		BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder;
+		String natio  = "korea";
+		String encNation = bcrypt.encode(nation);
+		
+		for(int i = 0 ; i < 10; i ++) {
+			encNation
+			System.out.println("%s : ");
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+	}
+
+}
